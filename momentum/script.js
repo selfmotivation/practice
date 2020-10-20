b3c76fadf928fade924287e2a07c9ac6776ca024
Selfmotivation <mira55@yandex.ru>
2020-10-20 19:51:02 +0400

    init: the task is cloned, some code added

---
+++ momentum/script.js
@@ -0,0 +1,46 @@
+// DOM Elements
+const time = document.querySelector('.time'),
+    greeting = document.querySelector('.greeting'),
+    name = document.querySelector('.name'),
+    focus = document.querySelector('.focus');
+
+// Show Time
+function showTime() {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    // Set AM or PM
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+    // 12hr Format
+    hour = hour % 12 || 12;
+
+    // Output Time
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+    setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+// Set Background and Greeting
+function setBgGreet() {
+    let today = new Date(),
+        hour = today.getHours();
+
+    if (hour < 12) {
+        // Morning
+    } else if (hour < 18) {
+        // Afternoon
+    } else {
+        // Evening
+    }
+}
+
+// Run
+showTime();
