450b33ac4c8a464a4364051d89fe6fea42073e6e
Selfmotivation <mira55@yandex.ru>
2020-10-21 23:58:29 +0400

    add: base task functionality

--- momentum/script.js
+++ momentum/script.js
@@ -2,11 +2,42 @@
 const time = document.querySelector('.time'),
     greeting = document.querySelector('.greeting'),
     name = document.querySelector('.name'),
-    focus = document.querySelector('.focus');
+    focus = document.querySelector('.focus'),
+    currentMonth = document.querySelector('.month'),
+    currentDay = document.querySelector('.day');
+
+// Date values
+let months = {
+    0: "января",
+    1: "февраля",
+    2: "марта",
+    3: "апреля",
+    4: "мая",
+    5: "июня",
+    6: "июля",
+    7: "августа",
+    8: "сентября",
+    9: "октября",
+    10: "ноября",
+    11: "декабря",
+}
+
+let daysOfTheWeek = {
+    0: "Воскресенье",
+    1: "Понедельник",
+    2: "Вторник",
+    3: "Среда",
+    4: "Четверг",
+    5: "Пятница",
+    6: "Суббота"
+}
 
 // Show Time
 function showTime() {
     let today = new Date(),
+        day = today.getDate(),
+        dayOfTheWeek = today.getDay(),
+        month = today.getMonth(),
         hour = today.getHours(),
         min = today.getMinutes(),
         sec = today.getSeconds();
@@ -14,8 +45,9 @@ function showTime() {
     // Set AM or PM
     const amPm = hour >= 12 ? 'PM' : 'AM';
 
-    // 12hr Format
-    // hour = hour % 12 || 12;
+    // Output date
+    currentDay.innerHTML = daysOfTheWeek[dayOfTheWeek];
+    currentMonth.innerHTML = `${day} ${months[month]}`;
 
     // Output Time
     time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
@@ -70,9 +102,18 @@ function setName(e) {
         if (e.which == 13 || e.keyCode == 13) {
             localStorage.setItem('name', e.target.innerText);
             name.blur();
+            // Make sure the field is not empty
+            if (localStorage.getItem('name') === '') {
+                name.textContent = '[Enter name]';
+                localStorage.removeItem('name');
+            }
         }
     } else {
         localStorage.setItem('name', e.target.innerText);
+        if (localStorage.getItem('name') === '') {
+            name.textContent = '[Enter name]';
+            localStorage.removeItem('name');
+        }
     }
 }
 
@@ -92,16 +133,34 @@ function setFocus(e) {
         if (e.which == 13 || e.keyCode == 13) {
             localStorage.setItem('focus', e.target.innerText);
             focus.blur();
+            // Make sure the field is not empty
+            if (localStorage.getItem('focus') === '') {
+                focus.textContent = '[Enter focus]';
+                localStorage.removeItem('focus');
+            }
         }
     } else {
         localStorage.setItem('focus', e.target.innerText);
+        if (localStorage.getItem('focus') === '') {
+            focus.textContent = '[Enter focus]';
+            localStorage.removeItem('focus');
+        }
+    }
+}
+
+// Changing Text inside Focus, Name
+function changeText() {
+    if (this.textContent === '[Enter name]' || this.textContent === '[Enter focus]') {
+        this.textContent = '';
     }
 }
 
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
+name.addEventListener('click', changeText);
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
+focus.addEventListener('click', changeText);
 
 // Run
 showTime();
