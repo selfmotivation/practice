62eb0f217fe85f6303b58fa3acdb3c8b5ade2a8a
Selfmotivation <mira55@yandex.ru>
2020-10-23 21:05:51 +0400

    add: bg imgs change

--- momentum/script.js
+++ momentum/script.js
@@ -42,8 +42,6 @@ function showTime() {
         min = today.getMinutes(),
         sec = today.getSeconds();
 
-    // Set AM or PM
-    const amPm = hour >= 12 ? 'PM' : 'AM';
 
     // Output date
     currentDay.innerHTML = daysOfTheWeek[dayOfTheWeek];
@@ -53,6 +51,11 @@ function showTime() {
     time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
     setTimeout(showTime, 1000);
+
+    // Change Background Image Every Hour
+    if (min == 59 && sec == 59) {
+        setTimeout(getImage, 1000);
+    }
 }
 
 // Add Zeros
@@ -67,22 +70,20 @@ function setBgGreet() {
 
     if (hour < 6) {
         // Night
-        document.body.style.backgroundImage = "url('assets/images/night/01.jpg')";
+        document.body.style.backgroundImage = `url('./assets/images/${bgImagesList[hour]}')`;
         greeting.textContent = 'Good night, ';
-        document.body.style.color = 'white';
     } else if (hour < 12) {
         // Morning
-        document.body.style.backgroundImage = "url('assets/images/morning/01.jpg')";
+        document.body.style.backgroundImage = `url('./assets/images/${bgImagesList[hour]}')`;
         greeting.textContent = 'Good morning, ';
         } else if (hour < 18) {
         // Afternoon
-        document.body.style.backgroundImage = "url('assets/images/day/01.jpg')";
+        document.body.style.backgroundImage = `url('./assets/images/${bgImagesList[hour]}')`;
         greeting.textContent = 'Good afternoon, ';
     } else {
         // Evening
-        document.body.style.backgroundImage = "url('assets/images/evening/01.jpg')";
+        document.body.style.backgroundImage = `url('./assets/images/${bgImagesList[hour]}')`;
         greeting.textContent = 'Good evening, ';
-        document.body.style.color = 'white';
     }
 }
 
@@ -100,19 +101,28 @@ function setName(e) {
     if (e.type === 'keypress') {
         // Make sure Enter is pressed
         if (e.which == 13 || e.keyCode == 13) {
-            localStorage.setItem('name', e.target.innerText);
-            name.blur();
             // Make sure the field is not empty
-            if (localStorage.getItem('name') === '') {
-                name.textContent = '[Enter name]';
-                localStorage.removeItem('name');
+            if (e.target.innerText === '') {
+                if (localStorage.getItem('name') === null) {
+                    name.textContent = '[Enter name]';
+                    localStorage.removeItem('name');
+                }
+            } else {
+                localStorage.setItem('name', e.target.innerText);
             }
+            name.blur();
         }
     } else {
-        localStorage.setItem('name', e.target.innerText);
-        if (localStorage.getItem('name') === '') {
-            name.textContent = '[Enter name]';
-            localStorage.removeItem('name');
+        if (e.target.innerText === '') {
+            if (localStorage.getItem('name') === null) {
+                name.textContent = '[Enter name]';
+            } else {
+                name.textContent = localStorage.getItem('name');
+            }
+        } else {
+            if (name.textContent !== '[Enter name]') {
+                localStorage.setItem('name', e.target.innerText);
+            }
         }
     }
 }
@@ -131,29 +141,105 @@ function setFocus(e) {
     if (e.type === 'keypress') {
         // Make sure Enter is pressed
         if (e.which == 13 || e.keyCode == 13) {
-            localStorage.setItem('focus', e.target.innerText);
-            focus.blur();
             // Make sure the field is not empty
-            if (localStorage.getItem('focus') === '') {
-                focus.textContent = '[Enter focus]';
-                localStorage.removeItem('focus');
+            if (e.target.innerText === '') {
+                if (localStorage.getItem('focus') === null) {
+                    focus.textContent = '[Enter focus]';
+                    // localStorage.removeItem('focus');
+                }
+            } else {
+                localStorage.setItem('focus', e.target.innerText);
             }
+            focus.blur();
         }
     } else {
-        localStorage.setItem('focus', e.target.innerText);
-        if (localStorage.getItem('focus') === '') {
-            focus.textContent = '[Enter focus]';
-            localStorage.removeItem('focus');
+        if (e.target.innerText === '') {
+            if (localStorage.getItem('focus') === null) {
+                focus.textContent = '[Enter focus]';
+                localStorage.removeItem('focus');
+            } else {
+                focus.textContent = localStorage.getItem('focus');
+            }
+        } else {
+            if (focus.textContent !== '[Enter focus]') {
+                localStorage.setItem('focus', e.target.innerText);
+            }
         }
     }
 }
 
 // Changing Text inside Focus, Name
 function changeText() {
-    if (this.textContent === '[Enter name]' || this.textContent === '[Enter focus]') {
         this.textContent = '';
+}
+
+// Background Image Change
+const base = './assets/images/';
+let bgImagesList = [];
+
+function viewBgImage(data) {
+    const body = document.querySelector('body');
+    const src = data;
+    const img = document.createElement('img');
+    img.src = src;
+    img.onload = () => {      
+        body.style.backgroundImage = `url(${src})`;
+    }; 
+}
+
+let i = new Date().getHours();
+
+function getImage() {
+    const index = ((i+1) % 24);
+    const imageSrc = base + bgImagesList[index];
+    viewBgImage(imageSrc);
+    i++;
+    btn.disabled = true;
+    setTimeout(function() {
+        btn.disabled = false
+    }, 1000);
+} 
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', getImage);
+
+// Set Background Images List
+function createBgImagesList() {
+    let dayTime;
+    let i = 0;
+    while (i < 24) {
+        if (i < 6) {
+            dayTime = 'morning/';
+            addImagesToList(dayTime);
+        } else if (i < 12) {
+            dayTime = 'day/';
+            addImagesToList(dayTime);
+        } else if (i < 18) {
+            dayTime = 'evening/';
+            addImagesToList(dayTime);
+        } else {
+            dayTime = 'night/';
+            addImagesToList(dayTime);
+        }
+        i++;
     }
 }
+
+// Add random images to list
+function addImagesToList(dayTime) {
+    let imgQuantity = 0;
+    imgQuantity = Math.floor(Math.random() * 20 + 1);
+    if (imgQuantity < 10) {
+        imgQuantity = '0' + imgQuantity;
+    }
+    // Check if there is the same Img in List already
+    if (bgImagesList.indexOf(`${dayTime}${imgQuantity}.jpg`) == -1) {
+        bgImagesList.push(`${dayTime}${imgQuantity}.jpg`);
+    } else addImagesToList(dayTime);
+}
+
+
+
 
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
@@ -164,6 +250,7 @@ focus.addEventListener('click', changeText);
 
 // Run
 showTime();
-setBgGreet();
 getName();
 getFocus();
+createBgImagesList();
+setBgGreet();
